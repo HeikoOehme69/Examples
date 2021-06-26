dfff898f9c0891b8c98429b21994516d5f72d174
HeikoOehme69 <86535118+HeikoOehme69@users.noreply.github.com>
2021-06-26 22:22:38 +0200

    Initial added
    Example for DI usage that results in anti pattern

---
+++ HeartbeatMonitor.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+
+namespace Example
+{
+    /// <summary>
+    /// Implementation of a hearbeat monitor
+    /// </summary>
+    class HeartbeatMonitor
+    {
+        IChannel _channel;
+
+        public HeartbeatMonitor(IChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public void Command()
+        {
+            _channel.Command("Send me a heartbeat");
+        }
+
+        public static void Register<TChannel>(ContainerBuilder builder)
+        {
+            builder.RegisterType<TChannel>().As<IChannel>();
+            builder.RegisterType<HeartbeatMonitor>();
+        }
+
+        public static void RegisterWithKey<TChannel>(ContainerBuilder builder, string key)
+        {
+            builder.RegisterType<TChannel>().Keyed<IChannel>(key);
+            builder.Register(ctx => new HeartbeatMonitor(ctx.ResolveKeyed<IChannel>(key)));
+        }
+    }
+}
